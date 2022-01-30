7476e2fc39b48f5c4b60d9d9892997911b368b00
laratonera68 <ehojedab@gmail.com>
2022-01-30 12:28:21 +0100

    Sprint_1 PHP Basic Nivel_1 Ejercicio_1 Codigo

--- Sprint_1/PHP/PHP bàsics (Tema 3)/index.php
+++ Sprint_1/PHP/PHP bàsics (Tema 3)/index.php
@@ -1,5 +1,25 @@
 <?php
 
+//---- EJERCICIO 1 ----
+
+echo "EJERCICIO 1"."<br>"."<br>";
+
+//Definir variable
+$a_integer =2022;
+//Imprimir variable concatenada
+echo "Integer = ".$a_integer.", ";
+
+$b_double = 2.022;
+echo "Double = ".$b_double."," ."\n";
+
+$c_string = "Soy una variable de tipo String";
+echo "String = ".$c_string .",".PHP_EOL;
+
+$d_boolean = true;
+echo "Boolean = ".$d_boolean ."<br>";
+
+//todas las variables concatenadas con salto de linea.
+echo $a_integer."<br>". $b_double."<br>". $c_string."<br>".$d_boolean."<br>"."<br>";
 
 
 ?>
