d44c9427fc8c8f3b6b0d53849efcff97f4e2c62c
laratonera68 <ehojedab@gmail.com>
2022-01-30 13:14:34 +0100

    Sprint_1 PHP Funciones y Estructuras de control Nivel_1 Ejercicios_2 -3 -4

--- Sprint_1/PHP/PHP Funciones y Estructuras de control (Tema 3)/index.php
+++ Sprint_1/PHP/PHP Funciones y Estructuras de control (Tema 3)/index.php
@@ -15,4 +15,19 @@ function par($num){
 
 echo par(30)."<br>"."<br>";
 
+//---- EJERCICIO 2 - 3 - 4 ----
+
+echo "EJERCICIOS 2 - 3 - 4"."<br>"."<br>";
+
+function contar($temporizador = 10){//Ejercicio 4
+
+    for ($i=2; $i<=$temporizador; $i= $i +2){//Ejercicio 2 
+
+         echo $i."<br>";
+    }
+}
+
+
+echo contar(13)."<br>";// Ejercicio 3 // Se puede pasar otro parametro diferente al default. 
+
 ?> 
