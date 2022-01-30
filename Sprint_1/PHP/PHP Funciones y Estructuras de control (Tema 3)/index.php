3c6c0e8b5180ae5ddbf04706cee8442cbf334708
laratonera68 <ehojedab@gmail.com>
2022-01-30 13:07:42 +0100

    Sprint_1 PHP Funciones y Estructuras de control Nivel_1 Ejercicio_1 codigo

--- Sprint_1/PHP/PHP Funciones y Estructuras de control (Tema 3)/index.php
+++ Sprint_1/PHP/PHP Funciones y Estructuras de control (Tema 3)/index.php
@@ -1,3 +1,18 @@
 <?php 
 
+//---- EJERCICIO 1 ----
+
+echo "EJERCICIO 1"."<br>"."<br>";
+
+
+function par($num){
+    if ($num%2 ==0){
+       echo "El numero ".$num." es par";
+       }else{
+       echo "El numero ".$num." es impar";
+       }  
+}     
+
+echo par(30)."<br>"."<br>";
+
 ?> 
